da9fbe6623ec3ee04d12ff32d8efc2218b0e83b2
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-07-26 14:26:44 +0530

    first commit

---
+++ src/main/java/rest/example/rest/Controller/Restcontroller.java
@@ -0,0 +1,11 @@
+package rest.example.rest.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Restcontroller {
+    @GetMapping(path = "/")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
