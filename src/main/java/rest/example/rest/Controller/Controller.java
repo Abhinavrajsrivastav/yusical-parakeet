fd6cb27e0e5277e72c0db4d9c466821064c5e6b7
Abhinav Raj Srivastava <abhiraj.srivast254@gmail.com>
2024-07-30 17:31:18 +0530

    30-07-2024-001

--- src/main/java/rest/example/rest/Controller/Controller.java
+++ src/main/java/rest/example/rest/Controller/Controller.java
@@ -3,7 +3,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Restcontroller {
+public class Controller {
     @GetMapping(path = "/")
 	public String helloWorld() {
 		return "Hello World";
